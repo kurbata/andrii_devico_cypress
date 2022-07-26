d683e54acdd5e55f172557618ee32ea73e63764a
kurbata <andrey.kyrbatov@gmail.com>
2022-07-26 13:38:10 +0300

    HomeWork2: Add tests for HomeWork2 (signup, login, logout)

--- cypress/e2e/logIn.spec.js
+++ cypress/e2e/logIn.spec.js
@@ -18,38 +18,37 @@ describe('UI tests for LogIn and logout', () => {
         cy.get(real_world_page.next_button).click()
     })
 
-        it('should validate error messages for the bank name', () => {
-            cy.get(real_world_page.bank_name_error).should('not.exist')
-            cy.get(real_world_page.input_bank_name_field).should('be.visible').click().blur()
-            cy.get(real_world_page.bank_name_error).should('be.visible')
-                .and('have.text', 'Enter a bank name')
-        })
+    it('should validate error messages for the bank name', () => {
+        cy.get(real_world_page.bank_name_error).should('not.exist')
+        cy.get(real_world_page.input_bank_name_field).should('be.visible').click().blur()
+        cy.get(real_world_page.bank_name_error).should('be.visible')
+            .and('have.text', 'Enter a bank name')
+    })
+    it('should validate error messages for the routing number', () => {
+        cy.get(real_world_page.routing_number_error).should('not.exist')
+        cy.get(real_world_page.input_routing_number_field).should('be.visible').click().blur()
+        cy.get(real_world_page.routing_number_error).should('be.visible')
+            .and('have.text', 'Enter a valid bank routing number')
+    })
 
-            it('should validate error messages for the routing number', () => {
-              cy.get(real_world_page.routing_number_error).should('not.exist')
-              cy.get(real_world_page.input_routing_number_field).should('be.visible').click().blur()
-              cy.get(real_world_page.routing_number_error).should('be.visible')
-                  .and('have.text', 'Enter a valid bank routing number')
-          })
+    it('should validate error messages for the account number', () => {
+        cy.get(real_world_page.account_number_error).should('not.exist')
+        cy.get(real_world_page.input_account_number_field).should('be.visible').click().blur()
+        cy.get(real_world_page.account_number_error).should('be.visible')
+            .and('have.text', 'Enter a valid bank account number')
+    })
 
-          it('should validate error messages for the account number', () => {
-                cy.get(real_world_page.account_number_error).should('not.exist')
-                cy.get(real_world_page.input_account_number_field).should('be.visible').click().blur()
-                cy.get(real_world_page.account_number_error).should('be.visible')
-                    .and('have.text', 'Enter a valid bank account number')
-            })
+    it('should allow the user to complete the login action with valid bank data', () => {
+        cy.get(real_world_page.input_bank_name_field).type('testBank')
+        cy.get(real_world_page.input_routing_number_field).type('111000025')
+        cy.get(real_world_page.input_account_number_field).type('0123456789')
+        cy.get(real_world_page.save_button).click()
+        cy.get(real_world_page.done_button).click()
+    })
 
-        it('should allow the user to complete the login action with valid bank data', () => {
-                cy.get(real_world_page.input_bank_name_field).type('testBank')
-                cy.get(real_world_page.input_routing_number_field).type('111000025')
-                cy.get(real_world_page.input_account_number_field).type('0123456789')
-                cy.get(real_world_page.save_button).click()
-                cy.get(real_world_page.done_button).click()
-            })
-
-            it('should allow the user to logout', () => {
-                cy.intercept('POST', '/logout').as('logout')
-                cy.get(real_world_page.log_out_button).should('be.visible').click()
-                cy.get(sign_in_page.title_text).should('be.visible').and('have.text', 'Sign in')
-            })
+    it('should allow the user to logout', () => {
+        cy.intercept('POST', '/logout').as('logout')
+        cy.get(real_world_page.log_out_button).should('be.visible').click()
+        cy.get(sign_in_page.title_text).should('be.visible').and('have.text', 'Sign in')
+    })
 })
